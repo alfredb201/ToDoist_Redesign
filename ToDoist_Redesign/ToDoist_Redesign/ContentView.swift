9d15b8469b2a5d89a36a429e63a04d57a34cb154
alfredb201 <77019508+alfredb201@users.noreply.github.com>
2022-11-22 12:35:15 +0100

    Add files via upload

---
+++ ToDoist_Redesign/ToDoist_Redesign/ContentView.swift
@@ -0,0 +1,149 @@
+//
+//  ContentView.swift
+//  ToDoist_Redesign
+//
+//  Created by Alfonso Buonaguro on 17/11/22.
+//
+
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
+
+struct ContentView: View {
+    @State var text: String
+    @State var showInfoModalView = false
+    @State var title: String
+    @State var note: String
+    @State var date = Date()
+    @Environment(\.colorScheme) var colorScheme
+    var body: some View {
+        
+        TabView(selection: .constant(1)) {
+            NavigationView{
+                Text("")
+                    .searchable(text: $text)
+                    .navigationTitle("Today")
+                    .toolbar{
+                        Button(action: {
+                            showInfoModalView = true
+                        }, label: {
+                            Image(systemName: "plus")
+                            
+                        })
+                        .sheet(isPresented: $showInfoModalView) {
+                            VStack (alignment: .leading) {
+                                
+                                NavigationView {
+                                    Form {
+                                        Section (header: Text("Title")){
+                                            TextField("Title", text: $title)
+                                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                .navigationTitle("New task")
+                                                .toolbar{
+                                                    ToolbarItem (placement: .cancellationAction){
+                                                        Button("Cancel",action: {
+                                                            showInfoModalView.toggle()
+                                                        })
+                                                    }
+                                                    ToolbarItem(placement: .navigationBarTrailing){
+                                                        Button("Add",action: {})
+                                                    }
+                                                }
+                                            
+                                        }
+                                        
+                                        Section (header: Text("Note")){
+                                            TextField("Note", text: $note)
+                                            DatePicker ("Date", selection: $date,displayedComponents: .date)
+                                                .datePickerStyle(.graphical)
+                                        }
+                                    }
+                                }
+                            }
+                            .onTapGesture {
+                                hideKeyboard()
+                            }
+                        }
+                    }
+            }.tabItem {
+                Image(systemName: "calendar")
+                Text("Today") }.tag(1)
+            
+            NavigationView{
+                Text("")
+                    .searchable(text: $text)
+                    .navigationTitle("Inbox")
+                    .toolbar{
+                        Button(action: {
+                            showInfoModalView = true
+                        }, label: {
+                            Image(systemName: "plus")
+                            
+                        })
+                        .sheet(isPresented: $showInfoModalView) {
+                            VStack (alignment: .leading) {
+                                
+                                NavigationView {
+                                    Form {
+                                        Section (header: Text("Title")){
+                                            TextField("Title", text: $title)
+                                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                                .navigationTitle("New task")
+                                                .toolbar{
+                                                    ToolbarItem (placement: .cancellationAction){
+                                                        Button("Cancel",action: {
+                                                            showInfoModalView.toggle()
+                                                        })
+                                                    }
+                                                    ToolbarItem(placement: .navigationBarTrailing){
+                                                        Button("Add",action: {})
+                                                    }
+                                                }
+                                            
+                                        }
+                                        
+                                        Section (header: Text("Note")){
+                                            TextField("Note", text: $note)
+                                            DatePicker ("Date", selection: $date,displayedComponents: .date)
+                                                .datePickerStyle(.graphical)
+                                        }
+                                    }
+                                }
+                            }
+                            .onTapGesture {
+                                hideKeyboard()
+                            }
+                        }
+                    }
+            }.tabItem {
+                Image(systemName: "tray")
+                Text("Inbox") }.tag(2)
+            
+            NavigationView{
+                Text("Tab Content 3")
+                    .searchable(text: $text)
+                    .navigationTitle("Done")
+            }.tabItem {
+                Image(systemName: "checkmark.rectangle.portrait")
+                Text("Done") }.tag(3)
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(text: "", title: "", note: "")
+
+        
+    }
+}
---
+++ ToDoist_Redesign/ToDoist_Redesign/ToDoist_RedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoist_RedesignApp.swift
+//  ToDoist_Redesign
+//
+//  Created by Alfonso Buonaguro on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoist_RedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(text: "", title: "", note: "")
+        }
+    }
+}
